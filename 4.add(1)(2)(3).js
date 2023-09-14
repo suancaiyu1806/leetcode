721b48b35cfc8b5b3dcd3fb1576cebd1592d2865
vectordong <vectordong@tencent.com>
2023-09-14 22:11:01 +0800

    feat: 实现题解一

--- 4.add(1)(2)(3).js
+++ 4.add(1)(2)(3).js
@@ -1,6 +1,6 @@
 /**
  * 请实现一个 add 函数，满足以下功能
- * 
+ *
  * add(1); 	// 1
  * add(1)(2);  	// 3
  * add(1)(2)(3)；  // 6
@@ -8,3 +8,17 @@
  * add(1, 2)(3);   // 6
  * add(1, 2, 3);   // 6
  */
+
+/**
+ * result 1
+ * 存在一些问题，需要多一次无参调用
+ */
+let paramsList = [];
+const add = (...arg) => {
+  paramsList = [...paramsList, ...arg];
+  return arg.length ? add : paramsList.reduce((x, y) => x + y, 0);
+};
+
+console.log(add(1, 2, 3)());
+console.log(add(1, 2)(3)());
+console.log(add(1)(2)(3)());
